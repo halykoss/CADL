c0f5e66c800165e78e696d03de275be58538c250
Federico Pennino <federico.p98@gmail.com>
2020-08-02 18:50:21 +0200

    Input da input.sample

--- bin/main.ml
+++ bin/main.ml
@@ -16,22 +16,23 @@ let tail_text =  "let matchTailType tuple = let (exp,value) = tuple in match exp
 let print_ctx k v bo = "(" ^k ^", " ^ v ^ ");\n" ^ bo;;
 let print_type a b = a ^ " | " ^  b;;
         let _ =
-          let lexbuf = Lexing.from_channel stdin in
-          try 
-                let result = Parser.main Lexer.token lexbuf in
-                  let (a,b) = Grammar.analyzeInput result in 
-                  "type typed = " ^ (List.fold_right print_type (Hashtbl.find_all Grammar.newTypes "type") "Lambda of typed * typed | None;;\n")   |> print_endline;
-                  "let ctx = [ \n" ^ (Hashtbl.fold print_ctx Grammar.ctx "];;\n") |> print_endline;
-                  head_text |> print_endline;
-                  tail_text |> print_endline;
-                  SS.iter Grammar.iter_formulas a;
-                  SS.iter Grammar.iter_rules b;
-                    print_newline();
-                    flush stdout; 
-          with 
-          Lexer.LexerException ->
-            exit 0
-          | Stdlib.Parsing.Parse_error -> 
-            Lexer.print_error "error" "Parser" lexbuf ;
-            exit 0
+          let in_channel = open_in "input.sample" in
+            let lexbuf = Lexing.from_channel in_channel in
+            try 
+                  let result = Parser.main Lexer.token lexbuf in
+                    let (a,b) = Grammar.analyzeInput result in 
+                    "type typed = " ^ (List.fold_right print_type (Hashtbl.find_all Grammar.newTypes "type") "Lambda of typed * typed | None;;\n")   |> print_endline;
+                    "let ctx = [ \n" ^ (Hashtbl.fold print_ctx Grammar.ctx "];;\n") |> print_endline;
+                    head_text |> print_endline;
+                    tail_text |> print_endline;
+                    SS.iter Grammar.iter_formulas a;
+                    SS.iter Grammar.iter_rules b;
+                      print_newline();
+                      flush stdout; 
+            with 
+            Lexer.LexerException ->
+              exit 0
+            | Stdlib.Parsing.Parse_error -> 
+              Lexer.print_error "error" "Parser" lexbuf ;
+              exit 0
           
